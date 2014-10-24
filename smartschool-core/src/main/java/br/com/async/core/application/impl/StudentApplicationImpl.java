b3edaf8db48de7368d058f10d6f132c6acfb506d
Rondinelli Mesquita <rondinellimesquitas@gmail.com>
2014-10-24 19:36:33 -0300

    Finish Student Controller

---
+++ smartschool-core/src/main/java/br/com/async/core/application/impl/StudentApplicationImpl.java
@@ -0,0 +1,49 @@
+package br.com.async.core.application.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.async.core.application.StudentApplication;
+import br.com.async.core.entities.Student;
+import br.com.async.core.repository.StudentRepository;
+
+@Service("studentApplicationImpl")
+@Transactional
+public class StudentApplicationImpl implements StudentApplication{
+
+	@Autowired
+	@Qualifier("studentRepositoryImpl")
+	private StudentRepository repository;
+
+	@Override
+	public boolean save(Student entity) {
+		return repository.save(entity);
+	}
+
+	@Override
+	public boolean update(Student entity) {
+		return repository.update(entity);
+	}
+
+	@Override
+	public boolean delete(Student entity) {
+		return repository.delete(entity);
+	}
+
+	@Override
+	public Student findByCode(Integer code) {
+		return repository.findByCode(code);
+	}
+
+	@Override
+	public List<Student> list() {
+		return repository.list();
+	}
+	
+	
+
+}
---
+++ smartschool-core/src/main/java/br/com/async/core/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package br.com.async.core.repository;
+
+import br.com.async.core.entities.Student;
+
+public interface StudentRepository extends AbstractRepository<Student, Integer>{
+
+}
