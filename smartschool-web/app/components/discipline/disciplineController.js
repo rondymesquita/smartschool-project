ff3bec5f8469bf297a319aefc6c91eb34d222a33
Rondinelli Mesquita <rondinellimesquitas@gmail.com>
2015-01-05 15:04:12 -0300

    disciplines - pagination

--- smartschool-web/app/components/discipline/disciplineController.js
+++ smartschool-web/app/components/discipline/disciplineController.js
@@ -1,9 +1,9 @@
 
 angular
 .module('SmartschoolApp')
-.controller('DisciplineController', ['$scope', 'DisciplineService','constants','toast','ngTableParams', disciplineController]);
+.controller('DisciplineController', ['$scope', '$filter', 'DisciplineService','constants','toast','ngTableParams', disciplineController]);
 
-function disciplineController($scope,   disciplineService,  constants,  toast, ngTableParams) {
+function disciplineController($scope, $filter, disciplineService,  constants,  toast, ngTableParams) {
 
     $scope.title = "Disciplinas";
     $scope.disciplines = [];
@@ -28,11 +28,15 @@ function disciplineController($scope,   disciplineService,  constants,  toast, n
 
             $scope.tableParams = new ngTableParams({
                 page: 1,            // show first page
-                count: 10           // count per page
+                count: 10,           // count per page
+                sorting: {
+                    code: 'desc'     // initial sorting
+                }
             }, {
                 total: $scope.disciplines.length, // length of data
                 getData: function($defer, params) {
-                    $defer.resolve($scope.disciplines.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                    var orderedData = params.sorting() ? $filter('orderBy')($scope.disciplines, params.orderBy()) : $scope.disciplines;
+                    $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
                 }
             });
 
