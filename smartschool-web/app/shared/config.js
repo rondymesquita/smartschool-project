fad22cae40b9392dc6cd9c6874f046ce31fc41ae
Rondinelli Mesquita <rondinellimesquitas@gmail.com>
2015-01-15 10:04:49 -0300

    Fix modal color

--- smartschool-web/app/shared/config.js
+++ smartschool-web/app/shared/config.js
@@ -1,5 +1,5 @@
 angular.module('SmartschoolApp').constant('config',{
-    url: 'http://localhost:9090/smartschool-ws/',
+    url: 'http://192.168.1.8:8080/smartschool-ws/',
     loginUri: 'api/login/',
     professorsUri: 'api/professors/',
     disciplinesUri: 'api/disciplines/'
