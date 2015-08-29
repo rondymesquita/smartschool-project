4133676c282400d05238f8707c9c4fcab5cb8e93
Rondy Mesquita <rondinellimesquitas@gmail.com>
2015-08-28 21:14:45 -0300

    [MOBILE TEST] Fix Console class

--- smartschool-mobile-test/src/test/java/br/com/utils/Console.java
+++ smartschool-mobile-test/src/test/java/br/com/utils/Console.java
@@ -5,11 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
-import javafx.application.Platform;
 import br.com.async.server.ServerRunnable;
 
 public class Console {
@@ -76,13 +73,13 @@ public class Console {
 	}
 
 
-	public static void runSafe(final Runnable runnable) {
-		Objects.requireNonNull(runnable, "runnable");
-		if (Platform.isFxApplicationThread()) {
-			runnable.run();
-		} else {
-			Platform.runLater(runnable);
-		}
-	}
+//	public static void runSafe(final Runnable runnable) {
+//		Objects.requireNonNull(runnable, "runnable");
+//		if (Platform.isFxApplicationThread()) {
+//			runnable.run();
+//		} else {
+//			Platform.runLater(runnable);
+//		}
+//	}
 	
 }
